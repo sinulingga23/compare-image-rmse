5aac1c4d5d98bdacc978c997e857d36f98db94b2
= <sinulinggatwo@gmail.com>
2021-03-14 08:51:34 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace MetopelP6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_bandingkan_gambar = new System.Windows.Forms.Button();
+            this.btn_upl_gambar2 = new System.Windows.Forms.Button();
+            this.btn_upl_gambar1 = new System.Windows.Forms.Button();
+            this.gb_gambar2 = new System.Windows.Forms.GroupBox();
+            this.gambar2 = new System.Windows.Forms.PictureBox();
+            this.gb_gambar1 = new System.Windows.Forms.GroupBox();
+            this.gambar1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.hasil_rmse = new System.Windows.Forms.Label();
+            this.btn_matriks_rgb_gambar1 = new System.Windows.Forms.Button();
+            this.btn_matriks_rgb_gambar2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.gb_gambar2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gambar2)).BeginInit();
+            this.gb_gambar1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gambar1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_bandingkan_gambar);
+            this.groupBox1.Controls.Add(this.btn_upl_gambar2);
+            this.groupBox1.Controls.Add(this.btn_upl_gambar1);
+            this.groupBox1.Controls.Add(this.gb_gambar2);
+            this.groupBox1.Controls.Add(this.gb_gambar1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(790, 250);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Menu Utama";
+            // 
+            // btn_bandingkan_gambar
+            // 
+            this.btn_bandingkan_gambar.Location = new System.Drawing.Point(344, 221);
+            this.btn_bandingkan_gambar.Name = "btn_bandingkan_gambar";
+            this.btn_bandingkan_gambar.Size = new System.Drawing.Size(101, 23);
+            this.btn_bandingkan_gambar.TabIndex = 5;
+            this.btn_bandingkan_gambar.Text = "Bandingkan";
+            this.btn_bandingkan_gambar.UseVisualStyleBackColor = true;
+            this.btn_bandingkan_gambar.Click += new System.EventHandler(this.btn_bandingkan_gambar_Click);
+            // 
+            // btn_upl_gambar2
+            // 
+            this.btn_upl_gambar2.Location = new System.Drawing.Point(548, 195);
+            this.btn_upl_gambar2.Name = "btn_upl_gambar2";
+            this.btn_upl_gambar2.Size = new System.Drawing.Size(101, 23);
+            this.btn_upl_gambar2.TabIndex = 4;
+            this.btn_upl_gambar2.Text = "Upload Gambar 2";
+            this.btn_upl_gambar2.UseVisualStyleBackColor = true;
+            this.btn_upl_gambar2.Click += new System.EventHandler(this.btn_upl_gambar2_Click);
+            // 
+            // btn_upl_gambar1
+            // 
+            this.btn_upl_gambar1.Location = new System.Drawing.Point(129, 196);
+            this.btn_upl_gambar1.Name = "btn_upl_gambar1";
+            this.btn_upl_gambar1.Size = new System.Drawing.Size(101, 23);
+            this.btn_upl_gambar1.TabIndex = 3;
+            this.btn_upl_gambar1.Text = "Upload Gambar 1";
+            this.btn_upl_gambar1.UseVisualStyleBackColor = true;
+            this.btn_upl_gambar1.Click += new System.EventHandler(this.btn_upl_gambar1_Click);
+            // 
+            // gb_gambar2
+            // 
+            this.gb_gambar2.Controls.Add(this.gambar2);
+            this.gb_gambar2.Location = new System.Drawing.Point(405, 19);
+            this.gb_gambar2.Name = "gb_gambar2";
+            this.gb_gambar2.Size = new System.Drawing.Size(380, 170);
+            this.gb_gambar2.TabIndex = 2;
+            this.gb_gambar2.TabStop = false;
+            this.gb_gambar2.Text = "Gambar 2";
+            // 
+            // gambar2
+            // 
+            this.gambar2.Location = new System.Drawing.Point(6, 19);
+            this.gambar2.Name = "gambar2";
+            this.gambar2.Size = new System.Drawing.Size(368, 145);
+            this.gambar2.TabIndex = 1;
+            this.gambar2.TabStop = false;
+            // 
+            // gb_gambar1
+            // 
+            this.gb_gambar1.Controls.Add(this.gambar1);
+            this.gb_gambar1.Location = new System.Drawing.Point(7, 20);
+            this.gb_gambar1.Name = "gb_gambar1";
+            this.gb_gambar1.Size = new System.Drawing.Size(380, 170);
+            this.gb_gambar1.TabIndex = 0;
+            this.gb_gambar1.TabStop = false;
+            this.gb_gambar1.Text = "Gambar 1";
+            // 
+            // gambar1
+            // 
+            this.gambar1.Location = new System.Drawing.Point(6, 19);
+            this.gambar1.Name = "gambar1";
+            this.gambar1.Size = new System.Drawing.Size(368, 145);
+            this.gambar1.TabIndex = 0;
+            this.gambar1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.hasil_rmse);
+            this.groupBox2.Location = new System.Drawing.Point(19, 269);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(618, 161);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Hasil";
+            // 
+            // hasil_rmse
+            // 
+            this.hasil_rmse.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.hasil_rmse.Location = new System.Drawing.Point(44, 60);
+            this.hasil_rmse.Name = "hasil_rmse";
+            this.hasil_rmse.Size = new System.Drawing.Size(523, 47);
+            this.hasil_rmse.TabIndex = 0;
+            this.hasil_rmse.Text = "-";
+            this.hasil_rmse.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btn_matriks_rgb_gambar1
+            // 
+            this.btn_matriks_rgb_gambar1.Location = new System.Drawing.Point(663, 278);
+            this.btn_matriks_rgb_gambar1.Name = "btn_matriks_rgb_gambar1";
+            this.btn_matriks_rgb_gambar1.Size = new System.Drawing.Size(128, 21);
+            this.btn_matriks_rgb_gambar1.TabIndex = 6;
+            this.btn_matriks_rgb_gambar1.Text = "Matriks RGB Gambar 1";
+            this.btn_matriks_rgb_gambar1.UseVisualStyleBackColor = true;
+            this.btn_matriks_rgb_gambar1.Click += new System.EventHandler(this.btn_matriks_rgb_gambar1_Click);
+            // 
+            // btn_matriks_rgb_gambar2
+            // 
+            this.btn_matriks_rgb_gambar2.Location = new System.Drawing.Point(663, 305);
+            this.btn_matriks_rgb_gambar2.Name = "btn_matriks_rgb_gambar2";
+            this.btn_matriks_rgb_gambar2.Size = new System.Drawing.Size(128, 21);
+            this.btn_matriks_rgb_gambar2.TabIndex = 7;
+            this.btn_matriks_rgb_gambar2.Text = "Matriks RGB Gambar 2";
+            this.btn_matriks_rgb_gambar2.UseVisualStyleBackColor = true;
+            this.btn_matriks_rgb_gambar2.Click += new System.EventHandler(this.btn_matriks_rgb_gambar2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 442);
+            this.Controls.Add(this.btn_matriks_rgb_gambar2);
+            this.Controls.Add(this.btn_matriks_rgb_gambar1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Pencocokan 2 Gambar - RMSE";
+            this.groupBox1.ResumeLayout(false);
+            this.gb_gambar2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gambar2)).EndInit();
+            this.gb_gambar1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gambar1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox gb_gambar2;
+        private System.Windows.Forms.GroupBox gb_gambar1;
+        private System.Windows.Forms.PictureBox gambar1;
+        private System.Windows.Forms.PictureBox gambar2;
+        private System.Windows.Forms.Button btn_upl_gambar2;
+        private System.Windows.Forms.Button btn_upl_gambar1;
+        private System.Windows.Forms.Button btn_bandingkan_gambar;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_matriks_rgb_gambar1;
+        private System.Windows.Forms.Button btn_matriks_rgb_gambar2;
+        private System.Windows.Forms.Label hasil_rmse;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MetopelP6
+{
+    public partial class Form1 : Form
+    {
+        private string pathHasil1 = @"D:\HASIL\MatriksRGB_Gambar1.txt";
+        private string pathHasil2 = @"D:\HASIL\MatriksRGB_Gambar2.txt";
+        private OpenFileDialog ofdGambar1;
+        private OpenFileDialog ofdGambar2;
+        private double[,] matriks1;
+        private double[,] matriks2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ofdGambar1 = new OpenFileDialog();
+            ofdGambar2 = new OpenFileDialog();
+            matriks1 = new double[5000, 5000];
+            matriks2 = new double[5000, 5000];
+            btn_matriks_rgb_gambar1.Enabled = false;
+            btn_matriks_rgb_gambar2.Enabled = false;
+        }
+
+
+
+        private void btn_upl_gambar1_Click(object sender, EventArgs e)
+        {
+            ofdGambar1.Filter = "Image Files (*.jpg;*.jpeg;*.png) | *.jpg;*.jpeg;*.png";
+
+            if (ofdGambar1.ShowDialog() == DialogResult.OK)
+            {
+                Image temp = Image.FromFile(ofdGambar1.FileName);
+                gambar1.Image = new Bitmap(temp, gb_gambar1.Width - 10, gb_gambar1.Height - 10);
+            }
+            hasil_rmse.Text = "-";
+        }
+
+        private void btn_upl_gambar2_Click(object sender, EventArgs e)
+        {
+            ofdGambar2.Filter = "Image Files (*.jpg;*.jpeg;*.png;) | *.jpg;*.jpeg;*.png";
+            if (ofdGambar2.ShowDialog() == DialogResult.OK)
+            {
+                Image temp = Image.FromFile(ofdGambar2.FileName);
+                gambar2.Image = new Bitmap(temp, gb_gambar2.Width - 10, gb_gambar2.Height - 10);
+            }
+            hasil_rmse.Text = "-";
+        }
+
+        private void simpanHasilKeFile(double[,] matriks, int width, int height, string path)
+        {
+            string hasil = "";
+            for (int i = 0; i < width; i++)
+            {
+                hasil += "|";
+                for (int j = 0; j < height; j++)
+                {
+                    hasil += matriks[i, j].ToString() + "|";
+                }
+                hasil += "\n";
+            }
+
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine(hasil);
+            }
+
+        }
+
+        private void btn_bandingkan_gambar_Click(object sender, EventArgs e)
+        {
+            Image t1 = Image.FromFile(ofdGambar1.FileName);
+            Bitmap tmp1 = new Bitmap(t1, 64, 64);
+
+            Image t2 = Image.FromFile(ofdGambar2.FileName);
+            Bitmap tmp2 = new Bitmap(t2, 64, 64);
+
+            double rmse = 0;
+            for (int i = 0; i < tmp1.Width; i++)
+            {
+                for (int j = 0; j < tmp1.Height; j++)
+                {
+                    // mendapatkan value RGB dari tiap gambar
+                    double pixel_gambar1 = tmp1.GetPixel(i, j).R + tmp1.GetPixel(i, j).G + tmp1.GetPixel(i, j).B;
+                    double pixel_gambar2 = tmp2.GetPixel(i, j).R + tmp2.GetPixel(i, j).G + tmp2.GetPixel(i, j).B;
+ 
+                    // simpan nilai RGB tiap pixel ke dalam matriks
+                    matriks1[i, j] = pixel_gambar1;
+                    matriks2[i, j] = pixel_gambar2;
+
+                    // menjumlahkan error yang terjadi
+                    if (!double.IsNaN(pixel_gambar1))
+                    {
+                        if (!double.IsNaN(pixel_gambar2))
+                        {
+                            rmse += (pixel_gambar2 - pixel_gambar1) * (pixel_gambar2 - pixel_gambar1);
+                        }
+                    }
+
+                }
+            }
+            rmse /= 64 * 64;
+            rmse = Math.Sqrt(rmse);
+
+            if (double.Equals(rmse, (double)0))
+            {
+                hasil_rmse.Text = "KEDUA GAMBAR SAMA";
+            }
+            else if (rmse > (double)0 && rmse <= (double)0.01)
+            {
+                hasil_rmse.Text = "KEDUA GAMBAR MIRIP";
+            }
+            else if (rmse > (double)0.01)
+            {
+                hasil_rmse.Text = "KEDUA GAMBAR BERBEDA";
+            }
+
+            simpanHasilKeFile(matriks1, 64, 64, pathHasil1);
+            simpanHasilKeFile(matriks2, 64, 64, pathHasil2);
+            btn_matriks_rgb_gambar1.Enabled = true;
+            btn_matriks_rgb_gambar2.Enabled = true;
+
+        }
+
+        private void btn_matriks_rgb_gambar1_Click(object sender, EventArgs e)
+        {
+            Process.Start(pathHasil1);
+        }
+
+        private void btn_matriks_rgb_gambar2_Click(object sender, EventArgs e)
+        {
+            Process.Start(pathHasil2);
+        }
+    }
+}
